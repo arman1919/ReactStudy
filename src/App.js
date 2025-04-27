a096958634f5410097b2325bc13d636c75c8f527
arman1919 <arman192001@mail.ru>
2025-04-27 22:22:40 +0400

    Lesson 8

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [city, setCity] = useState('');
+
+  const addUser = () => {
+    const newUser = {
+      name,
+      age,
+      city
+    };
+    setUsers([...users, newUser]);
+    // Очистить инпуты после добавления
+    setName('');
+    setAge('');
+    setCity('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="task-1">
+      <div className="user-card">
+        <input
+          className="user-card-item"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="User name"
+        />
+        <input
+          className="user-card-item"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          placeholder="User age"
+        />
+        <input
+          className="user-card-item"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="User city"
+        />
+        <div>
+          <button className="user-card-button" onClick={addUser}>
+            Add user
+          </button>
+        </div>
+      </div>
+
+      <div className="user-card-list">
+        {users.map((user, index) => (
+          <div key={index} className="user-card-list-item">
+            <div className="user-card-list-item-name">{user.name}</div>
+            <div className="user-card-list-item-age">{user.age}</div>
+            <div className="user-card-list-item-city">{user.city}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
