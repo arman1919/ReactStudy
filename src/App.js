b67c48b1badf91b7c5fbb92364c925f513309be9
Arman <arman.raf2001@gmail.com>
2025-04-28 18:51:36 +0400

    Less 11

--- src/App.js
+++ src/App.js
@@ -1,64 +1,11 @@
-import { useState } from 'react';
 import './App.css';
-
+import Header from './Header';
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  const [name, setName] = useState('');
-  const [age, setAge] = useState('');
-  const [city, setCity] = useState('');
-
-  const addUser = () => {
-    const newUser = {
-      name,
-      age,
-      city
-    };
-    setUsers([...users, newUser]);
-    // Очистить инпуты после добавления
-    setName('');
-    setAge('');
-    setCity('');
-  };
-
+  
   return (
-    <div className="task-1">
-      <div className="user-card">
-        <input
-          className="user-card-item"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="User name"
-        />
-        <input
-          className="user-card-item"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-          placeholder="User age"
-        />
-        <input
-          className="user-card-item"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="User city"
-        />
-        <div>
-          <button className="user-card-button" onClick={addUser}>
-            Add user
-          </button>
-        </div>
-      </div>
-
-      <div className="user-card-list">
-        {users.map((user, index) => (
-          <div key={index} className="user-card-list-item">
-            <div className="user-card-list-item-name">{user.name}</div>
-            <div className="user-card-list-item-age">{user.age}</div>
-            <div className="user-card-list-item-city">{user.city}</div>
-          </div>
-        ))}
-      </div>
+    <div className='app'>
+        <Header />
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+
+import './Header.css';
+
+
+function Header() {
+  
+  return (
+    <div className='header'>
+      <ul>
+        <li>header 1</li>
+        <li>header 2</li>
+        <li>header 3</li>
+      </ul>  
+    </div>
+  );
+}
+
+export default Header;
