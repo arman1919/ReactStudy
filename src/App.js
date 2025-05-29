46992127a1893eaabe575200fc6a596ba645f5ba
arman1919 <arman192001@mail.ru>
2025-05-29 22:15:29 +0400

    Less 19

--- src/App.js
+++ src/App.js
@@ -2,26 +2,19 @@ import './App.css';
 import Header from './components/Header/Header';
 import NavBar from './components/Navbar/Nav-bar';
 import Profile from './components/Profile/Profile';
-import Task from './Task';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dialogs from './components/Dialogs/Dialogs';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="app-wraper">
-              <Header />
-              <NavBar />
-              <Profile />
-            </div>
-          }
-        />
-        <Route path="/task" element={<Task />} />
-      </Routes>
-    </Router>
+    <div className="app-wraper">
+      <Header />
+      <NavBar />
+      <div className="app-wraper-content">
+          {/* <Profile /> */}
+          <Dialogs/>
+      </div>
+
+    </div>
   );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,11 @@
 
 
-import classNames from './Profile.module.css';
+import s from './Profile.module.css';
 import MyPosts from './My_posts/MyPosts';
 
 const Profile = () => {
     return (
-      <div className={classNames.content}>
+      <div>
         <div>
           <img src="https://blog.adobe.com/en/publish/2024/10/14/media_1ca79b205381242c5f8beaaee2f0e1cfb2aa8f324.png?width=750&format=png&optimize=medium" alt="" />
         </div>
