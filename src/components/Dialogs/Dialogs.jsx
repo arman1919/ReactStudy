eaedeaadcd98fffb170c76608bfdc65e17393512
arman1919 <arman192001@mail.ru>
2025-06-07 15:48:45 +0400

    Less 25

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,18 +19,35 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+    let dialogsData = [
+        {id: 1, name: "John"},
+        {id: 2, name: "Jane"},
+        {id: 3, name: "Jim"},
+        {id: 4, name: "Alex"},
+        {id: 5, name: "Bob"},
+        {id: 6, name: "Charlie"},
+        {id: 7, name: "David"},
+        {id: 8, name: "Ethan"},
+        {id: 9, name: "Frank"},
+        {id: 10, name: "George"},
+    ]
+
+    let messagesData = [
+        {id: 1, message: "Hello"},
+        {id: 2, message: "How are you?"},
+        {id: 3, message: "I'm fine, thank you!"},
+        {id: 4, message: "What are you doing?"},
+        {id: 5, message: "I'm doing nothing."},
+    ]
+
     return (
         <div>
             <div className={s.dialogs}>
                 <div className={s.dialogsItems}>
-                    <DialogItem name="John" id={1}/>
-                    <DialogItem name="Jane" id={2}/>
-                    <DialogItem name="Jim" id={3}/>
+                    {dialogsData.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>)}
                 </div>
                 <div className={s.messages}>    
-                    <Message message="Hello" />
-                    <Message message="How are you?" />
-                    <Message message="I'm fine, thank you!" />
+                    {messagesData.map(message => <Message message={message.message}/>)}
                 </div>
             </div>
         </div>
--- src/components/Profile/My_posts/MyPosts.jsx
+++ src/components/Profile/My_posts/MyPosts.jsx
@@ -4,6 +4,12 @@ import s from './MyPosts.module.css';
 import Post from './post/Post';
 
 const MyPosts = () => {
+
+  let postsData = [
+    {id: 1, message: "Hello", likesCount: 10},
+    {id: 2, message: "How are you?", likesCount: 45},
+  ]
+
     return (
         <div className={s.postsBlock}>
           <h3>My post</h3>
@@ -16,8 +22,7 @@ const MyPosts = () => {
             </div>
           </div>
           <div className={s.posts}>
-            <Post value="post 1" likesCount={10}/>
-            <Post value="post 2" likesCount={45}/>
+            {postsData.map(post => <Post value={post.message} likesCount={post.likesCount}/>)}
           </div>
         </div>
     );
