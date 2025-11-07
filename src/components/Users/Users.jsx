fbc2eda304d0e487d6b10d21fdaf8a74dc6aad2c
Arman <arman.raf2001@gmail.com>
2025-11-07 12:01:19 +0400

    Less 51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,12 +1,21 @@
+import axios from 'axios';
 import s from './Users.module.css';
 
 let Users = (props) => {
+
+    if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        })
+    }
+
+
     return (
         <div className={s.usersContainer}>
-            {
+            {                   
                 props.users.map( u => <div className={s.users} key={u.id}>
                     <div className={s.usersPhoto}>
-                        <img src="https://static.vecteezy.com/system/resources/previews/000/439/863/large_2x/vector-users-icon.jpg" alt="" />
+                        <img src={u.photos != null && u.photos.small != null ? u.photos.small : "https://static.vecteezy.com/system/resources/previews/000/439/863/large_2x/vector-users-icon.jpg"} alt="" />
                         {
                             u.followed ? 
                             <button onClick={() => props.unfollow(u.id)}>Unfollow</button> :
@@ -16,11 +25,11 @@ let Users = (props) => {
                     <div className={s.usersInfo}>
                         <div className={s.usersName}>
                             <span>{u.name}</span>
-                            <span>{u.status}</span>
+                            <span>{u.status != null ? u.status : "No status"}</span>
                         </div>
                         <div className={s.usersLocation}>
-                            <span>{u.location.city}</span>
-                            <span>{u.location.country}</span>
+                            <span>{"City"}</span>
+                            <span>{"Country"}</span>
                         </div>
                     </div>
                 </div>)
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -5,13 +5,7 @@ const SET_USERS = 'SET_USERS'
 
 
 let initialState = {
-  users: [
-    { id: 1, followed: false, name: "Dmitry", status: "I'm fine, thank you!", location: {city: "Minsk", country: "Belarus" } },
-    { id: 2, followed: true, name: "Max", status: "I'm fine, thank you!", location: {city: "Moscow", country: "Russia" } },
-    { id: 3, followed: false, name: "Ivan", status: "I'm fine, thank you!", location: {city: "St.Петербург", country: "Russia" } },
-    { id: 4, followed: true, name: "Sergey", status: "I'm fine, thank you!", location: {city: "Kazan", country: "Russia" } },
-    { id: 5, followed: false, name: "Pavel", status: "I'm fine, thank you!", location: {city: "Novosibirsk", country: "Russia" } },
-  ],
+  users: [],
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -25,7 +19,7 @@ const usersReducer = (state = initialState, action) => {
       return stateCopy2
     
     case SET_USERS:
-      return {...state, users: [...state.users, ...action.users]}
+      return {...state, users: action.users}
 
     default:
       return state;
