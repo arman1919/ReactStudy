8f39edc9ef7c06fb4043d2994ef47ef224ab8246
Arman <arman.raf2001@gmail.com>
2025-11-16 11:32:49 +0400

    Less 53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,45 @@
 import axios from 'axios';
 import s from './Users.module.css';
+import React from 'react';
 
-let Users = (props) => {
-    let gerUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items)
-            })
-        }
+class Users extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
 
-    return (
-        <div className={s.usersContainer}>
-            <button onClick={gerUsers}>Get users</button>
-            {                   
-                props.users.map( u => <div className={s.users} key={u.id}>
-                    <div className={s.usersPhoto}>
-                        <img src={u.photos != null && u.photos.small != null ? u.photos.small : "https://static.vecteezy.com/system/resources/previews/000/439/863/large_2x/vector-users-icon.jpg"} alt="" />
-                        {
-                            u.followed ? 
-                            <button onClick={() => props.unfollow(u.id)}>Unfollow</button> :
-                            <button onClick={() => props.follow(u.id)}>Follow</button>
-                        }
-                    </div>
-                    <div className={s.usersInfo}>
-                        <div className={s.usersName}>
-                            <span>{u.name}</span>
-                            <span>{u.status != null ? u.status : "No status"}</span>
+    render() {
+         return (
+            <div className={s.usersContainer}>
+                {                   
+                    this.props.users.map( u => <div className={s.users} key={u.id}>
+                        <div className={s.usersPhoto}>
+                            <img src={u.photos != null && u.photos.small != null ? u.photos.small : "https://static.vecteezy.com/system/resources/previews/000/439/863/large_2x/vector-users-icon.jpg"} alt="" />
+                            {
+                                u.followed ? 
+                                <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button> :
+                                <button onClick={() => this.props.follow(u.id)}>Follow</button>
+                            }
                         </div>
-                        <div className={s.usersLocation}>
-                            <span>{"City"}</span>
-                            <span>{"Country"}</span>
+                        <div className={s.usersInfo}>
+                            <div className={s.usersName}>
+                                <span>{u.name}</span>
+                                <span>{u.status != null ? u.status : "No status"}</span>
+                            </div>
+                            <div className={s.usersLocation}>
+                                <span>{"City"}</span>
+                                <span>{"Country"}</span>
+                            </div>
                         </div>
-                    </div>
-                </div>)
-            }
-        </div>
-    );
+                    </div>)
+                }
+            </div>
+        );
+    }
 }
 
 export default Users;
